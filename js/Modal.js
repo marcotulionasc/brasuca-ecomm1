779401b1d02286e1caccb0d768a33761d5550cf0
marcotulionasc <marconascpriv@gmail.com>
2024-06-30 19:33:26 -0300

    adicionando modals da home

---
+++ js/Modal.js
@@ -0,0 +1,26 @@
+function toggleModal(modalId) {
+    const modal = document.getElementById(modalId);
+    const mainContent = document.querySelector('.main-content');
+    
+    modal.classList.toggle('hidden');
+    mainContent.classList.toggle('blurred');
+}
+
+function toggleSidebarCart(sidebarId) {
+    const sidebar = document.getElementById(sidebarId);
+    sidebar.classList.toggle('translate-x-full');
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('.toggle-login').forEach(button => {
+        button.addEventListener('click', () => toggleModal('loginModal'));
+    });
+
+    document.querySelectorAll('.toggle-cadastro').forEach(button => {
+        button.addEventListener('click', () => toggleModal('cadastroModal'));
+    });
+
+    document.querySelectorAll('.toggle-cart').forEach(button => {
+        button.addEventListener('click', () => toggleSidebarCart('cartSidebar'));
+    });
+});
