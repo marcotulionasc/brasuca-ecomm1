da1415a3621b91f52f831e0a8d56e915e9498fbf
marcotulionasc <marconascpriv@gmail.com>
2024-07-08 09:15:21 -0300

    :sparkles: adicionado protocolo de segurança

--- js/Config/config.js
+++ js/Config/config.js
@@ -1,6 +1,6 @@
 // config.js
 const config = {
-    apiBaseUrl: "http://189.79.31.77:9090/api" // Default to development URL
+    apiBaseUrl: "https://59e8-189-79-31-77.ngrok-free.app/api" // Default to development URL
 };
 
 export default config;
