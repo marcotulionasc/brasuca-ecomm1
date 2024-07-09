e5eabf99432e9c94b48d7db69160c6ac9e2e7968
marcotulionasc <marconascpriv@gmail.com>
2024-07-08 21:43:26 -0300

    teste

--- js/Config/config.js
+++ js/Config/config.js
@@ -1,6 +1,6 @@
 // config.js
 const config = {
-    apiBaseUrl: "https://d390-189-79-31-77.ngrok-free.app/api" // URL to PRODUCTION
+    apiBaseUrl: "https://a512-2804-1b3-a7c2-dd3a-907a-498e-e4f5-aafd.ngrok-free.app/api" // URL to PRODUCTION
 };
 
 export default config;
