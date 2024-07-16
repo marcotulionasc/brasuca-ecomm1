b93c533090eec4e7a2be08571958f12f6b6369ad
marcotulionasc <marconascpriv@gmail.com>
2024-07-15 23:58:06 -0300

    :sparkles: atualizando

--- js/loader.js
+++ js/loader.js
@@ -3,5 +3,5 @@ document.addEventListener("DOMContentLoaded", function() {
     setTimeout(function() {
         document.getElementById('preloader-active').style.display = 'none';
         document.getElementById('main-content').style.display = 'block';
-    }, 4000); // 3 seconds
+    }, 3000); // 3 seconds
 });
