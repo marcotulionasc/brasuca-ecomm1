28dbae9007903da0d1a1fa1ed7809d304edfd35b
marcotulionasc <marconascpriv@gmail.com>
2024-08-29 15:27:38 -0300

    :sparkles: subindo URL dev

--- js/Configuration.js
+++ js/Configuration.js
@@ -2,7 +2,7 @@ const config = {
     production: true, // Altere para false em desenvolvimento
     urls: {
         production: "https://assuring-amazingly-monitor.ngrok-free.app",
-        development: "http://187.34.212.10:9090"
+        development: "http://201.92.184.45:9090"
     },
     getBaseUrl: function() {
         return this.production ? this.urls.production : this.urls.development;
