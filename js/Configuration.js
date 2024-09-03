0803258bc5e8887a09f1f1ff1cc8e45fea1ac569
marcotulionasc <marconascpriv@gmail.com>
2024-09-03 18:51:17 -0300

    :sparkles: ajustes no profile.html

--- js/Configuration.js
+++ js/Configuration.js
@@ -1,8 +1,8 @@
 const config = {
-    production: true, // Altere para false em desenvolvimento
+    production: false, // Altere para false em desenvolvimento
     urls: {
       production: "https://backend-ingressar.onrender.com",
-        development: "http://191.205.83.233:9090"
+        development: "http://localhost:8080"
     },
     getBaseUrl: function() {
         return this.production ? this.urls.production : this.urls.development;
